deca299c261a245745cb545c2b0ad60cc06e48c7
Guillermo Llancaqueo <guillermo.llancaqueo@gmail.com>
2019-12-10 14:43:54 -0300

    [FIBONACCI]: se añade solución estructurada

--- recursividadStructurada.js
+++ recursividadStructurada.js
@@ -3,6 +3,18 @@
 // 1, 1, 2, 3, 5, 8
 
 function fibonacci (i) {
+  let z = 1
+  let y = 1
+  let p = 0
 
+  for(let x = 0; x < i; x++) {
+    y = z
+    z = p + y
+    p = y
+  }
+
+  return z
 }
+
+console.log('hola: ', fibonacci(6))
 
